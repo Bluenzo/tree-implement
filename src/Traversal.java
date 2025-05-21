12479efff1131eef6603d48d93ee683e283b29ce
Bluenzo <80460870+Bluenzo@users.noreply.github.com>
2025-05-21 12:42:22 -0700

    TreeNode notes

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,34 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
+
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(15);
+
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+    root.right = new TreeNode(15);
+    root.right.left = new TreeNode(-3);
+    root.right.right = new TreeNode(5);
+
+    root.right.right.right = new TreeNode(22);
+
+    preorder(root);
+  }
+
+  public static void preorder(TreeNode current){ //void because we don't print anything
+    //if null: return
+    if(current == null) return; //current is the TreeNode.  
+
+    //print data
+    System.out.println(current.data); //current.data is to print the data inside current.
+
+    //search left
+    preorder(current.left); 
+
+    //search right
+    preorder(current.right);
 
   }
 }
